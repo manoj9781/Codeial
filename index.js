5cb9d113c696773affbb7fbfd3541afa1b5b8ff1
Manoj Singh <manojsinghrana467@gmail.com>
2021-08-05 13:54:10 +0530

    Setup Static Files

--- index.js
+++ index.js
@@ -2,18 +2,20 @@ const express = require('express');
 const app = express();
 const port = 8000;
 const expressLayouts = require('express-ejs-layouts');
-
+app.use(express.static('./assets'));
 app.use(expressLayouts);
 
-// Use express router
 
+
+// Use express router
 app.use('/', require('./routes'));
 
 //Setup up view engine
-
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
+
+// Start the server
 app.listen(port, function (err) {
     if (err) {
         console.log(`Error in ruuning the server: ${err}`);
