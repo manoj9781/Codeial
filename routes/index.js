2076d1709dbda3f00738fb1d8a95203e93320752
Manoj Singh <manojsinghrana467@gmail.com>
2021-08-05 11:35:37 +0530

    Home controller is added

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+const homeController = require('../controller/home_controller');
+
+router.get('/', homeController.home);
+
 console.log(`Router is loaded`);
 
 module.exports = router;
