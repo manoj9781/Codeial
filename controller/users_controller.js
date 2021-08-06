82eade06dd66e48576ffc050f0c19f6bf3bd52ae
Manoj Singh <manojsinghrana467@gmail.com>
2021-08-06 18:15:40 +0530

    User Created

--- controller/users_controller.js
+++ controller/users_controller.js
@@ -1,3 +1,5 @@
+const User = require('../models/user');
+
 module.exports.profile = function (req, res) {
     // return res.end('<h1> User Profile</h1>')
     return res.render('profile', {
@@ -24,7 +26,29 @@ module.exports.signIn = function (req, res) {
 // Get sign Up data
 module.exports.create = function (req, res) {
     //TODO later
-}
+    if (req.body.password != req.body.confirm_password) {
+        return res.redirect('back');
+    }
+    User.findOne({ email: req.body.email }, function (err, user) {
+        if (err) {
+            console.log("Error in finding the user");
+            return;
+        }
+        if (!user) {
+            User.create(req.body, function (err, user) {
+                if (err) {
+                    console.log("Error in creating the user");
+                    return;
+                }
+                return res.redirect('/users/sign-in');
+            })
+        }
+        else {
+            return res.redirect('back');
+        }
+    });
+    
+};
 //sign In
 module.exports.createSession = function (req, res) {
     //TODO later
