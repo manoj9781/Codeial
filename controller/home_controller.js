c75dbf5841c2e7d9b9dd3791e23a8e5481d12fef
Manoj Singh <manojsinghrana467@gmail.com>
2021-08-14 17:51:44 +0530

    Show comments and user details on the home Page

--- controller/home_controller.js
+++ controller/home_controller.js
@@ -1,25 +1,32 @@
-
 const Post = require('../models/post');
 
 module.exports.home = function (req, res) {
-    // console.log(req.cookies);
-    // res.cookie('id', 27);
-// return res.end("<h1>Server is up for codeial</h1>");
-    
-    // Only for posts
-//     Post.find({}, function (err, posts) {
-//         return res.render('home', {
-//             title: "Home",
-//             posts:posts,
-//         });
-//    })
-    
-    // For User name and Deatils
+  // console.log(req.cookies);
+  // res.cookie('id', 27);
+  // return res.end("<h1>Server is up for codeial</h1>");
 
-    Post.find({}).populate('user').exec(function (err, posts) {
-        return res.render('home', {
-            title: "Codeial | Home",
-            posts: posts,
-        });
+  // Only for posts
+  //     Post.find({}, function (err, posts) {
+  //         return res.render('home', {
+  //             title: "Home",
+  //             posts:posts,
+  //         });
+  //    })
+
+  // For User name and Deatils
+
+  Post.find({})
+      .populate('user')
+      .populate({
+          path: 'comments',
+          populate: {
+              path:'user',
+          },
+      })
+    .exec(function (err, posts) {
+      return res.render('home', {
+        title: 'Codeial | Home',
+        posts: posts,
+      });
     });
 };
