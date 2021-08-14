ffa5f0eb936c0b80c082bbc5690b8678498fb941
Manoj Singh <manojsinghrana467@gmail.com>
2021-08-14 12:24:49 +0530

    User comment and user details on home Page done

--- controller/home_controller.js
+++ controller/home_controller.js
@@ -4,11 +4,22 @@ const Post = require('../models/post');
 module.exports.home = function (req, res) {
     // console.log(req.cookies);
     // res.cookie('id', 27);
-    Post.find({}, function (err, posts) {
+// return res.end("<h1>Server is up for codeial</h1>");
+    
+    // Only for posts
+//     Post.find({}, function (err, posts) {
+//         return res.render('home', {
+//             title: "Home",
+//             posts:posts,
+//         });
+//    })
+    
+    // For User name and Deatils
+
+    Post.find({}).populate('user').exec(function (err, posts) {
         return res.render('home', {
-            title: "Home",
-            posts:posts,
+            title: "Codeial | Home",
+            posts: posts,
         });
-   })
-    // return res.end("<h1>Server is up for codeial</h1>");
+    });
 };
