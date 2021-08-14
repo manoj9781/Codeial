6fca007fdf472163205dfae31d21e953f59e39ea
Manoj Singh <manojsinghrana467@gmail.com>
2021-08-14 12:14:22 +0530

    Show users post in home page

--- controller/home_controller.js
+++ controller/home_controller.js
@@ -1,8 +1,14 @@
+
+const Post = require('../models/post');
+
 module.exports.home = function (req, res) {
-    console.log(req.cookies);
-    res.cookie('id', 27);
-    return res.render('home', {
-        title: "Home",
-    });
+    // console.log(req.cookies);
+    // res.cookie('id', 27);
+    Post.find({}, function (err, posts) {
+        return res.render('home', {
+            title: "Home",
+            posts:posts,
+        });
+   })
     // return res.end("<h1>Server is up for codeial</h1>");
 };
