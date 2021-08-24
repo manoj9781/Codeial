2558ce0b9f7c1ab12771de8ced2710cea81e7ac1
Manoj Singh <manojsinghrana467@gmail.com>
2021-08-24 23:18:37 +0530

    New post form submit using AJAX request

---
+++ assets/js/home_posts.js
@@ -0,0 +1,23 @@
+{
+    // Method to sunmit the form data for new Post in AJAX
+    let createPost = function () {
+        let newPostForm = $('#new-post-from');
+        newPostForm.submit(function (e) {
+            e.preventDefault();
+
+            $.ajax({
+                type: 'post',
+                url: '/posts/create',
+                data: newPostForm.serialize(),
+                success: function (data) {
+                    console.log(data);
+                },
+                error: function (error) {
+                    console.log(error.responseText);
+                },
+            });
+        });
+    }
+    createPost();
+   
+}
--- controller/posts_controller.js
+++ controller/posts_controller.js
@@ -16,10 +16,19 @@ const Comment = require('../models/comment');
 
 module.exports.create = async function (req, res) {
   try {
-    await Post.create({
+   let post =  await Post.create({
       content: req.body.content,
       user: req.user.id,
-    });
+   });
+	  
+	  if (req.xhr) {
+		  return res.status(200).json({
+			  data: {
+				  post: post
+			  },
+			  message: 'Post Created',
+		  });
+	  }
     req.flash('success', 'Post Published!');
     return res.redirect('back');
   } catch (error) {
@@ -49,12 +58,12 @@ module.exports.destroy = async function (req, res) {
     let post = await Post.findById(req.params.id);
     if (post.user == req.user.id) {
       post.remove();
-        await Comment.deleteMany({ post: req.params.id });
-        req.flash('success', "Post and associasted comments deleted");
+      await Comment.deleteMany({ post: req.params.id });
+      req.flash('success', 'Post and associasted comments deleted');
       return res.redirect('back');
     } else {
-        req.flash('success', "You are not authorised to delete the post");
-        return res.redirect('back');
+      req.flash('success', 'You are not authorised to delete the post');
+      return res.redirect('back');
     }
   } catch (error) {
     // req.flash('error', err);
