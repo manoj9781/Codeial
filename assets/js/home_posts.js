86be1285718048cbbdec55d085cefb7cc92ab316
Manoj Singh <manojsinghrana467@gmail.com>
2021-08-26 18:41:18 +0530

    Create a post in DOM

--- assets/js/home_posts.js
+++ assets/js/home_posts.js
@@ -10,7 +10,8 @@
 				url :'/posts/create',
 				data: newPostForm.serialize(),
 				success:function(data){
-					console.log(data);
+					let newPost = newPostDom(data.data.post);
+					$('#posts-list-container>ul').prepend(newPost);
 				},
 				error: function(error){
 					console.log(error);
@@ -19,4 +20,38 @@
 		});
 	}
 	createPost();
+	
+	// Method to create a post in DOM
+	
+	let newPostDom = function(post){
+		return $(`<li id="post-${post._id}">
+  <p>
+      
+      <small>
+          <a class="delete-post-button"  href="/posts/destroy/${ post.id}">X</a>
+      </small>
+      
+      ${ post.content }
+      <br>
+      <small>
+          ${post.user.name}
+      </small>
+  </p>
+  <div class="post-comments">
+      
+          <form action="/comments/create" method="POST">
+              <input type="text" name="content" placeholder="Type Here to add comment..." required>
+              <input type="hidden" name="post" value="${ post._id }" >
+              <input type="submit" value="Add Comment">
+          </form>
+
+      <div class="post-comments-list">
+          <ul id="post-comments-${ post._id }">
+             
+          </ul>
+      </div>
+  </div>
+  
+</li>`)
+	}
 }
