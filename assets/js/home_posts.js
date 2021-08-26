7451d83564b85c1b1ea220572866b0eefe00c7b6
Manoj Singh <manojsinghrana467@gmail.com>
2021-08-26 18:12:50 +0530

    Submit form data using ajax request

--- assets/js/home_posts.js
+++ assets/js/home_posts.js
@@ -1,23 +1,22 @@
 {
-    // Method to submit the form data for new Post in AJAX
-    let createPost = function () {
-        let newPostForm = $('#new-post-from');
-        newPostForm.submit(function (e) {
-            e.preventDefault();
-
-            $.ajax({
-                type: 'post',
-                url: '/posts/create',
-                data: newPostForm.serialize(),
-                success: function (data) {
-                    console.log(data);
-                },
-                error: function (error) {
-                    console.log(error.responseText);
-                },
-            });
-        });
-    }
-    createPost();
-   
+	// Method to submit the form data using ajax 
+	let createPost = function(){
+		let newPostForm = $('#new-post-form');
+		newPostForm.submit(function(e){
+			e.preventDefault();
+			
+			$.ajax({
+				type:'post',
+				url :'/posts/create',
+				data: newPostForm.serialize(),
+				success:function(data){
+					console.log(data);
+				},
+				error: function(error){
+					console.log(error);
+				}
+			});
+		});
+	}
+	createPost();
 }
