9ef7f6bb9d42caa7b9f9d9c4478d9cf5b51a2266
Manoj Singh <manojsinghrana467@gmail.com>
2021-08-28 23:43:58 +0530

    Code refactored and Added home posts comments

--- controller/comments_controller.js
+++ controller/comments_controller.js
@@ -1,92 +1,80 @@
 const Comment = require('../models/comment');
-
 const Post = require('../models/post');
 
-// module.exports.create = function (req, res) {
-//     Post.findById(req.body.post, function (err, post) {
-//         if (post) {
-//             Comment.create({
-//                 content: req.body.content,
-//                 post: req.body.post,
-//                 user: req.user._id,
-//             }, function (err, comment) {
-//                 if (err) {
-//                     console.log("Error");
-//                 }
-//                 post.comments.push(comment);
-//                 post.save();
-//                 res.redirect('/');
-//             });
-//         }
-//     });
-// };
+module.exports.create = async function(req, res){
 
-module.exports.create = async function (req, res) {
-    try {
+    try{
         let post = await Post.findById(req.body.post);
-        if (post) {
+
+        if (post){
             let comment = await Comment.create({
                 content: req.body.content,
                 post: req.body.post,
-                user: req.user._id,
+                user: req.user._id
             });
 
             post.comments.push(comment);
             post.save();
 
-            req.flash('success', "Comment Added");
-           return res.redirect('/');
+            if (req.xhr){
+                // Similar for comments to fetch the user's id!
+                comment = await comment.populate('user', 'name').execPopulate();
+    
+                return res.status(200).json({
+                    data: {
+                        comment: comment
+                    },
+                    message: "Post created!"
+                });
+            }
+
+
+            req.flash('success', 'Comment published!');
+
+            res.redirect('/');
         }
-    } catch (error) {
-        // console.log("Error", error);
-        req.flash('error', error);
-        return res.redirect('/');
-        
+    }catch(err){
+        req.flash('error', err);
+        return;
     }
+    
 }
 
 
-// module.exports.destroy = function (req, res) {
-//     Comment.findById(req.params.id, function (err, comment) {
-//         if (comment.user == req.user.id) {
-//             let postId = comment.post;
-//             comment.remove();
-//             Post.findByIdAndUpdate(postId, {
-//                 $pull: {
-//                     comments: req.params.id
-//                 }
-//             }, function (err, post) {
-//                 return res.redirect('back');
-//             })
-//         }
-//         else {
-//             return res.redirect('back');
-//         }
-//     });
-// };
+module.exports.destroy = async function(req, res){
 
-module.exports.destroy = async function (req, res) {
-    try {
+    try{
         let comment = await Comment.findById(req.params.id);
-        if (comment.user == req.user.id) {
+
+        if (comment.user == req.user.id){
+
             let postId = comment.post;
+
             comment.remove();
 
-            let post = Post.findByIdAndUpdate(postId, {
-                $pull: {
-                    comments: req.params.id,
-                }
-            });
-            req.flash('success', "Comment Deleted");
+            let post = Post.findByIdAndUpdate(postId, { $pull: {comments: req.params.id}});
+
+            // send the comment id which was deleted back to the views
+            if (req.xhr){
+                return res.status(200).json({
+                    data: {
+                        comment_id: req.params.id
+                    },
+                    message: "Post deleted"
+                });
+            }
+
+
+            req.flash('success', 'Comment deleted!');
+
             return res.redirect('back');
-        } else {
-            req.flash("success", "You cannot delete the comment");
+        }else{
+            req.flash('error', 'Unauthorized');
             return res.redirect('back');
         }
-    } catch (error) {
-        // console.log("Error", error);
-        req.flash('error', error);
-            return res.redirect('back');
-            // return;
+    }catch(err){
+        req.flash('error', err);
+        return;
     }
+    
 }
--- controller/posts_controller.js
+++ controller/posts_controller.js
@@ -1,84 +1,69 @@
 const Post = require('../models/post');
 const Comment = require('../models/comment');
 
-// module.exports.create = function (req, res) {
-//     Post.create({
-//         content: req.body.content,
-//         user: req.user._id,
-//     }, function (err, post) {
-//         if (err) {
-//             console.log("Error in creating the post");
-//             return;
-//         }
-//         return res.redirect('back');
-//     });
-// };
+module.exports.create = async function(req, res){
+    try{
+        let post = await Post.create({
+            content: req.body.content,
+            user: req.user._id
+        });
+        
+        if (req.xhr){
+            // if we want to populate just the name of the user (we'll not want to send the password in the API), this is how we do it!
+            post = await post.populate('user', 'name').execPopulate();
 
-module.exports.create = async function (req, res) {
-  try {
-   let post =  await Post.create({
-      content: req.body.content,
-      user: req.user.id,
-   });
-	  
-	  if (req.xhr) {
-		  return res.status(200).json({
-			  data: {
-				  post: post
-			  },
-			  message: 'Post Created',
-		  });
-	  }
-	 
-    req.flash('success', 'Post Published!');
-    return res.redirect('back');
-  } catch (error) {
-    // console.log('Error', error);
-    req.flash('error', err);
-    return res.redirect('back');
-  }
-};
+            return res.status(200).json({
+                data: {
+                    post: post
+                },
+                message: "Post created!"
+            });
+        }
 
-// module.exports.destroy = function (req, res) {
-//     Post.findById(req.params.id, function (err, post) {
-//         // .id means converting the object id into string
-//         if (post.user == req.user.id) {
-//             post.remove();
-//             Comment.deleteMany({ post: req.params.id }, function (err) {
-//                 return res.redirect('back');
-//             });
-//         }
-//         else {
-//             return res.redirect('back');
-//         }
-//     });
-// };
+        req.flash('success', 'Post published!');
+        return res.redirect('back');
 
-module.exports.destroy = async function (req, res) {
-  try {
-    let post = await Post.findById(req.params.id);
-    if (post.user == req.user.id) {
-      post.remove();
-      await Comment.deleteMany({ post: req.params.id });
-		
-		if(req.xhr){
-			return res.status(200).json({
-				data:{
-					post_id:req.params.id
-				},
-				message:"Post Deleted"
-			})
-		}
-		
-      req.flash('success', 'Post and associasted comments deleted');
-      return res.redirect('back');
-    } else {
-      req.flash('success', 'You are not authorised to delete the post');
-      return res.redirect('back');
+    }catch(err){
+        req.flash('error', err);
+        // added this to view the error on console as well
+        console.log(err);
+        return res.redirect('back');
     }
-  } catch (error) {
-    // req.flash('error', err);
-    console.log('Error', error);
-    return;
-  }
-};
+  
+}
+
+
+module.exports.destroy = async function(req, res){
+
+    try{
+        let post = await Post.findById(req.params.id);
+
+        if (post.user == req.user.id){
+            post.remove();
+
+            await Comment.deleteMany({post: req.params.id});
+
+
+            if (req.xhr){
+                return res.status(200).json({
+                    data: {
+                        post_id: req.params.id
+                    },
+                    message: "Post deleted"
+                });
+            }
+
+            req.flash('success', 'Post and associated comments deleted!');
+
+            return res.redirect('back');
+        }else{
+            req.flash('error', 'You cannot delete this post!');
+            return res.redirect('back');
+        }
+
+    }catch(err){
+        req.flash('error', err);
+        return res.redirect('back');
+    }
+    
+}
