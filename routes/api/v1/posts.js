bc53bdb12c85e516463656b7b8de56bec653dd73
Manoj Singh <manojsinghrana467@gmail.com>
2021-09-02 23:19:47 +0530

    Render Api on the webpage

---
+++ routes/api/v1/posts.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+const postApi = require('../../../controller/api/v1/posts_api');
+
+router.get('/', postApi.index);
+
+module.exports = router;
