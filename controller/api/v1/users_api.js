b9408f8b48bdc25e112bd5ea80bcac9cde2bdf06
Manoj Singh <manojsinghrana467@gmail.com>
2021-09-03 22:31:31 +0530

    Sign in Using jwt in Postman

---
+++ controller/api/v1/users_api.js
@@ -0,0 +1,27 @@
+const User = require('../../../models/user');
+const jwt = require('jsonwebtoken');
+
+module.exports.createSession = async function (req, res) {
+    try {
+        let user = await User.findOne({ email: req.body.email });
+
+        if (!user || user.password != req.body.password) {
+            return res.josn(422, {
+                message: "Invalid Username or Pasword",
+            });
+        }
+        return res.json(200, {
+            message: "Sign inSuccesful, here is your token, please keep it safe",
+            data: {
+                token: jwt.sign(user.toJSON(), 'codeial', {
+                    expiresIn: '10000'
+                })
+            },
+        });
+    } catch (error) {
+        console.log('****', err);
+        return res.json(500, {
+            message: 'Internal server Error',
+        });
+    }
+}
