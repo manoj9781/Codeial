997413ea0e9abd20402681f97c703779a33b6b86
Manoj Singh <manojsinghrana467@gmail.com>
2021-09-09 23:27:54 +0530

    Resolved the errors getting in the previous code

--- models/like.js
+++ models/like.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const likeschema = new mongoose.Schema(
+const likeSchema = new mongoose.Schema(
   {
     user: {
       type: mongoose.Schema.Types.ObjectId,
