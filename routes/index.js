511b40358d4933c7332b993e43fcd87b9ffa00a0
Manoj Singh <manojsinghrana467@gmail.com>
2021-09-09 23:28:31 +0530

    Routes for like is done

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,7 @@ router.get('/', homeController.home);
 router.use('/users', require('./users'));
 router.use('/posts', require('./posts'));
 router.use('/comments', require('./comments'));
+router.use('/likes', require('./like'));
 
 router.use('/api', require('./api'));
 
---
+++ routes/like.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+const likesController = require('../controller/likes_controller');
+
+router.post('/toggle', likesController.toggleLike);
+
+module.exports = router;
