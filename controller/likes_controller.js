77a60dc87ada66088d0744c815298c84b4132d81
Manoj Singh <manojsinghrana467@gmail.com>
2021-09-10 22:20:52 +0530

    Updates Likes controller

--- controller/likes_controller.js
+++ controller/likes_controller.js
@@ -1,49 +1,65 @@
-const Like = require('../models/like');
+const Like = require("../models/like");
+const Post =  require("../models/post");
 const Comment = require('../models/comment');
-const Post = require('../models/post');
 
-module.exports.toggleLike = async function (req, res) {
-  try {
-    //likes/toggle/?id=1232344&type=post
 
-    let likeable;
-    let deleted = false;
-    if (req.query.type == 'Post') {
-      likeable = await Post.findById(req.query.id).populate('likes');
-    } else {
-      likeable = await Comment.findById(req.query.id).populate('likes');
+module.exports.toggleLike = async function(req, res){
+    try{
+
+        // likes/toggle/?id=abcdef&type=Post
+        let likeable;
+        let deleted = false;
+
+
+        if (req.query.type == 'Post'){
+            likeable = await Post.findById(req.query.id).populate('likes');
+        }else{
+            likeable = await Comment.findById(req.query.id).populate('likes');
+        }
+
+
+        // check if a like already exists
+        let existingLike = await Like.findOne({
+            likeable: req.query.id,
+            onModel: req.query.type,
+            user: req.user._id
+        })
+
+        // if a like already exists then delete it
+        if (existingLike){
+            likeable.likes.pull(existingLike._id);
+            likeable.save();
+
+            existingLike.remove();
+            deleted = true;
+
+        }else{
+            // else make a new like
+
+            let newLike = await Like.create({
+                user: req.user._id,
+                likeable: req.query.id,
+                onModel: req.query.type
+            });
+
+            likeable.likes.push(newLike._id);
+            likeable.save();
+
+        }
+
+        return res.json(200, {
+            message: "Request successful!",
+            data: {
+                deleted: deleted
+            }
+        })
+
+
+
+    }catch(err){
+        console.log(err);
+        return res.json(500, {
+            message: 'Internal Server Error'
+        });
     }
-    // check if a like is already exist or not if exist remove it if not then add it
-    let existingLike = await Like.findOne({
-      likeable: req.query.id,
-      onModel: req.query.type,
-      user: req.user._id,
-    });
-
-    if (existingLike) {
-      likeable.likes.push(exixtingLike.id);
-      likeable.save();
-
-      existingLike.remove();
-      deleted = true;
-    } else {
-      let newLike = await Like.create({
-        user: req.user._id,
-        likeable: req.query.id,
-        onModel: req.query.type,
-      });
-      likeable.likes.push(Like._id);
-      likeable.save();
-    }
-    return res.json(200, {
-      message: 'Added succesfully',
-      data: {
-        deleted: deleted,
-      },
-    });
-  } catch (error) {
-    console.log('Error', error);
-    req.flash('error', 'Unaothorised');
-    return res.status(401).send('Unaothorised');
-  }
-};
+}
