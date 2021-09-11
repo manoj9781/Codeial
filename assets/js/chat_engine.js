64c59a45a27a4cd3dc8fb6ac90bd8dacc791018d
Manoj Singh <manojsinghrana467@gmail.com>
2021-09-12 00:05:38 +0530

    front end for socket.io

---
+++ assets/js/chat_engine.js
@@ -0,0 +1,19 @@
+class chatEngine {
+    constructor(chatBoxId, userEmail) {
+        this.chatBoxId = chatBoxId;
+        this.userEmail = userEmail;
+
+        this.socket = io.connect('http://localhost:5000');
+
+        if (this.userEmail) {
+            this.connectionHandler();
+        }
+    }
+
+
+    connectionHandler() {
+        this.socket.on('connect', function () {
+            console.log('Connection established using socket');
+        })
+    }
+}
