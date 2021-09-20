0c09f252caa05e395dbbb7940317f68ce13919af
Manoj Singh <manojsinghrana467@gmail.com>
2021-09-20 23:21:26 +0530

    Front-end of the chat Engine

--- assets/js/chat_engine.js
+++ assets/js/chat_engine.js
@@ -21,8 +21,47 @@ class chatEngine {
       });
       self.socket.on('user_joined', function (data) {
         console.log('A user joined', data);
-    })
-    
+      });
+    });
+
+    $('#send-message').click(function () {
+      let msg = $('#chat-message-input').val();
+
+      if (msg != '') {
+        self.socket.emit('sendMessage', {
+          message: msg,
+          user_email: self.userEmail,
+          chatRoom: 'codeial',
+        });
+      }
+    });
+
+    self.socket.on('recieveMessage', function (data) {
+      console.log('Message Recieved', data.message);
+
+      let newMessage = $('<li>');
+
+      let messageType = 'other-message';
+
+      if (data.user_email == self.userEmail) {
+        messageType = 'self-message';
+      }
+
+      newMessage.append($(
+        '<span>', {
+          html: data.message,
+        }
+      ));
+
+      newMessage.append($(
+        '<sub>', {
+          html: data.user_email,
+        }
+      ));
+
+      newMessage.addClass(messageType);
+
+      $('#chat-messages-list').append(newMessage);
     });
   }
 }
